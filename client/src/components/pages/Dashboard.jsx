9f8c1c57da6828db3bfeec42a2eacaef3aa4dd28
Cel3stt <mariannecelest@gmail.com>
2024-09-30 01:23:31 +0800

    Fixing Dashboard Contents

--- client/src/components/pages/Dashboard.jsx
+++ client/src/components/pages/Dashboard.jsx
@@ -39,9 +39,9 @@ const Dashboard = () => {
       <div className="hidden border-r bg-muted/40 md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-            <Link href="/" className="flex items-center gap-2 font-semibold">
+            <Link to="/dashboard" className="flex items-center gap-2 font-semibold">
               <Package2 className="h-6 w-6" />
-              <span className="">Acme Inc</span>
+              <span className="">Sample</span>
             </Link>
             <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
               <Bell className="h-4 w-4" />
@@ -51,43 +51,37 @@ const Dashboard = () => {
           <div className="flex-1">
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
               <Link
-                href="#"
+                to="/dashboard"
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
               >
                 <Home className="h-4 w-4" />
                 Dashboard
               </Link>
               <Link
-                href="#"
+                to="/dashboard"
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
               >
                 <ShoppingCart className="h-4 w-4" />
-                Orders
-                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                Employees
+                {/* <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
                   6
-                </Badge>
+                </Badge> */}
               </Link>
               <Link
-                href="#"
+                to='/dashboard'
                 className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
               >
                 <Package className="h-4 w-4" />
-                Products{" "}
+                Category
               </Link>
               <Link
                 href="#"
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
               >
                 <Users className="h-4 w-4" />
-                Customers
+                Users
               </Link>
-              <Link
-                href="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-              >
-                <LineChart className="h-4 w-4" />
-                Analytics
-              </Link>
+             
             </nav>
           </div>
         
@@ -192,7 +186,10 @@ const Dashboard = () => {
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
+              <Link to='/Profile'>
               <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              </Link>
+             
               <DropdownMenuSeparator />
               <DropdownMenuItem>Settings</DropdownMenuItem>
               <DropdownMenuItem>Support</DropdownMenuItem>
--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -9,10 +9,18 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+ 
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert"
 
 import axios from "axios"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+
+import { AlertCircle } from "lucide-react"
 
 
 export function Login() {
@@ -23,58 +31,80 @@ export function Login() {
 
  const navigate = useNavigate()
 
+ const [error,setError] = useState(null)
+
+ //to display the cookies in the cookies storage of console
+ axios.defaults.withCredentials=true;
+
  const handleSubmit = (event) => {
     event.preventDefault()
 
     axios.post('http://localhost:3000/auth/adminLogin', values)
     .then(result => {
-      navigate('/dashboard')
+      if(result.data.loginStatus){
+        navigate('/dashboard')
+      }else{
+        setError(result.data.Error)
+      }
     })
     .catch(err => console.log(err))
  }
 
     return(
-    <form onSubmit={handleSubmit}>
-        <div className="min-h-screen justify-center flex items-center">
-  
-  <Card className="w-full max-w-sm ">
-    <CardHeader>
-      <CardTitle className="text-2xl font-bold">Login</CardTitle>
-      <CardDescription>
-        Enter your email below to login to your account.
-      </CardDescription>
-    </CardHeader>
-    <CardContent className="grid gap-4">
-      
-      
-      <div className="grid gap-2">
-        <Label htmlFor="email">Email</Label>
-        <Input id="email" type="email" 
-        placeholder="m@example.com"
-         required
-         onChange = {(e) => setValues({...values, email: e.target.value})}
-          />
-      </div>
+   
 
-      {/* PASSWORD */}
-      <div className="grid gap-2">
-        <Label htmlFor="password">Password</Label>
-        <Input id="password" 
-        placeholder='password' 
+<form onSubmit={handleSubmit}>
+
+
+
+<div className="min-h-screen justify-center flex items-center">
+
+<Card className="w-full max-w-sm ">
+<div className="text-red-500 bold pl-5 pt-5 block">
+    {error && error}
+  </div>
+
+
+<CardHeader>
+  <CardTitle className="text-2xl font-bold">Login</CardTitle>
+  <CardDescription>
+    Enter your email below to login to your account.
+  </CardDescription>
+
+</CardHeader>
  
-        required
-        onChange={(e) => setValues({...values, password: e.target.value})}
-        />
-      </div>
-    </CardContent>
+<CardContent className="grid gap-4">
+  <div className="grid gap-2">
+    <Label htmlFor="email">Email</Label>
+    <Input id="email" type="email" 
+    placeholder="m@example.com"
+     required
+     onChange = {(e) => setValues({...values, email: e.target.value})}
+      />
+  </div>
 
-    <CardFooter>
-      <Button className="w-full">Sign in</Button>
-    </CardFooter>
-  </Card>
+  {/* PASSWORD */}
+  <div className="grid gap-2">
+    <Label htmlFor="password">Password</Label>
+    <Input id="password" 
+    placeholder='password' 
+
+    required
+    onChange={(e) => setValues({...values, password: e.target.value})}
+    />
+  </div>
+</CardContent>
+
+<CardFooter>
+  <Button className="w-full">Sign in</Button>
+</CardFooter>
+</Card>
 
 </div>
-    </form>
+</form>
+      
+   
+    
     )
 
 }
